751de44108ebc2f96f8bf6c94d3f6ecd19b353b5
jacebright <jacebright@gmail.com>
2025-01-31 00:22:04 -0600

    W03 Individual Activity

--- src/js/ProductListing.mjs
+++ src/js/ProductListing.mjs
@@ -2,9 +2,9 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
   return `<li class="product-card">
-  <a href="product_pages/index.html?product=${product.Id}">
+  <a href="../product_pages/index.html?product=${product.Id}">
   <img
-    src="${product.Image}"
+    src="${product.Images.PrimaryMedium}"
     alt="Image of ${product.Name}"
   />
   <h3 class="card__brand">${product.Brand.Name}</h3>
@@ -23,7 +23,7 @@ export default class ProductList {
   }
   async init() {
     // our dataSource will return a Promise...so we can use await to resolve it.
-    const list = await this.dataSource.getData();
+    const list = await this.dataSource.getData(this.category);
 
     const filteredList = list.filter((tent) => tent.Id != "989CG" && tent.Id != "880RT");
     // render the list
